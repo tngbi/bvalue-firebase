04a82f4ebcfe4f86b023920e65473642d01aa57a
tngbi <tngbia@gmail.com>
2025-02-27 00:06:02 +0000

    ok
    ok

--- functions/index.js
+++ functions/index.js
@@ -7,8 +7,16 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+// Remove unused imports
+// const { onRequest } = require("firebase-functions/v2/https");
+// const logger = require("firebase-functions/logger");
+
+// Example function (replace with your actual function)
+const functions = require("firebase-functions");
+
+exports.helloWorld = functions.https.onRequest((req, res) => {
+  res.send("Hello from Firebase Cloud Functions!");
+});
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
